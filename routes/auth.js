287023e07711f0f078256ae6695b57fe2ecd575c
Priyansh Rai <priyanshrai16@gmail.com>
2023-06-01 21:17:26 +0530

    Used express-validator for field validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,28 +1,49 @@
 const express = require("express");
+const { body, validationResult } = require("express-validator");
 const User = require("../models/User");
 
 const router = express.Router();
 
-// Create a user using POST on /api/auth/ No auth required.
+// Create a user using POST on /api/auth/user
 
 const createUser = async (userDetails) => {
     try {
         const user = await User.create(userDetails);
-        return user;
+        return {
+            success: true,
+            user,
+        };
     } catch (e) {
         return {
-            error: {
-                code: e.code,
-                name: e.name,
-                message: e.message,
-            },
+            errors: [
+                {
+                    code: e.code,
+                    name: e.name,
+                    message: e.message,
+                },
+            ],
         };
     }
 };
 
-router.post("/", async (req, res) => {
-    const user = await createUser(req.body);
-    res.send(user);
-});
+router.post(
+    "/user",
+    [
+        body("name").isLength({ min: 3 }),
+        body("email").isEmail(),
+        body("password").isLength({ min: 8 }),
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res
+                .status(400)
+                .json({ success: false, errors: errors.array() });
+        }
+
+        const result = await createUser(req.body);
+        res.json(result);
+    }
+);
 
 module.exports = router;
