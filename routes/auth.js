66b9f09b36f42243dc1247da6d4d7451d411a639
Priyansh Rai <priyanshrai16@gmail.com>
2023-06-03 10:46:45 +0530

    Used jwt for authentication

--- routes/auth.js
+++ routes/auth.js
@@ -2,11 +2,17 @@ const express = require("express");
 const { body, validationResult } = require("express-validator");
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+
+// Configuring dotenv for .env files
+dotenv.config();
+
+const JWT_SECRET = process.env.JWT_SECRET;
 
 const router = express.Router();
 
 // Create a user using POST on /api/auth/user
-
 const createUser = async (userDetails) => {
     try {
         const user = await User.create(userDetails);
@@ -53,8 +59,62 @@ router.post(
             email: req.body.email,
             password: passwordHash,
         });
+
+        // const data = {
+        //     user: {
+        //         id: result.user.id,
+        //     },
+        // };
+
+        // const authtoken = jwt.sign(data, JWT_SECRET);
         res.json(result);
     }
 );
 
+// Authenticate a user using POST /api/auth/login
+router.post(
+    "/login",
+    [
+        body("email").isEmail(),
+        body("password", "Please enter a password.").isLength({ min: 1 }),
+    ],
+    async (req, res) => {
+        // Getting the request validation result and returning errors if any
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            return res
+                .status(400)
+                .json({ success: false, errors: errors.array() });
+        }
+
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email });
+
+        // Checking if a user with provided email exists
+        if (!user) {
+            return res.status(400).json({
+                success: false,
+                error: {
+                    message: "Invalid login credentials. Please try again.",
+                },
+            });
+        }
+
+        // Comparing provided password with password hash stored in the database
+        const result = await bcrypt.compare(password, user.password);
+
+        if (!result) {
+            return res.status(400).json({
+                success: false,
+                error: {
+                    message: "Invalid login credentials. Please try again.",
+                },
+            });
+        }
+
+        res.json({ success: true, user });
+    }
+);
 module.exports = router;
